64745673182b5d893439b7ad415ea8fb01780275
Dmitry Norkin <nordmtr@gmail.com>
2020-04-07 12:43:55 +0300

    make_env function

--- atari_wrappers.py
+++ atari_wrappers.py
@@ -135,6 +135,48 @@ class PreprocessAtariObs(gym.ObservationWrapper):
         return observation
 
 
+class FrameBuffer(Wrapper):
+    def __init__(self, env, n_frames=4, dim_order='pytorch'):
+        """A gym wrapper that returns stacked `n_frames` instead of a single frame in non-Markovian processes"""
+        super(FrameBuffer, self).__init__(env)
+        self.dim_order = dim_order
+        if dim_order == 'tensorflow':
+            height, width, n_channels = env.observation_space.shape
+            obs_shape = [height, width, n_channels * n_frames]
+        elif dim_order == 'pytorch':
+            n_channels, height, width = env.observation_space.shape
+            obs_shape = [n_channels * n_frames, height, width]
+        else:
+            raise ValueError(
+                'dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
+        self.observation_space = Box(0.0, 1.0, obs_shape)
+        self.framebuffer = np.zeros(obs_shape, 'float32')
+
+    def reset(self):
+        """resets breakout, returns initial frames"""
+        self.framebuffer = np.zeros_like(self.framebuffer)
+        self.update_buffer(self.env.reset())
+        return self.framebuffer
+
+    def step(self, action):
+        """plays breakout for 1 step, returns frame buffer"""
+        new_img, reward, done, info = self.env.step(action)
+        self.update_buffer(new_img)
+        return self.framebuffer, reward, done, info
+
+    def update_buffer(self, img):
+        if self.dim_order == 'tensorflow':
+            offset = self.env.observation_space.shape[-1]
+            axis = -1
+            cropped_framebuffer = self.framebuffer[:, :, :-offset]
+        elif self.dim_order == 'pytorch':
+            offset = self.env.observation_space.shape[0]
+            axis = 0
+            cropped_framebuffer = self.framebuffer[:-offset]
+        self.framebuffer = np.concatenate(
+            [img, cropped_framebuffer], axis=axis)
+
+
 def PrimaryAtariWrap(env, clip_rewards=True):
     assert 'NoFrameskip' in env.spec.id
 
@@ -146,4 +188,24 @@ def PrimaryAtariWrap(env, clip_rewards=True):
         env = ClipRewardEnv(env)
 
     env = PreprocessAtariObs(env)
+    return env
+
+
+def make_env(ENV_NAME="BreakoutNoFrameskip-v4", clip_rewards=True, seed=None):
+    env = gym.make(ENV_NAME)  # create raw env
+
+    assert 'NoFrameskip' in env.spec.id
+
+    if seed is not None:
+        env.seed(seed)
+
+    env = MaxAndSkipEnv(env, skip=4)
+    env = EpisodicLifeEnv(env)
+    env = FireResetEnv(env)
+
+    if clip_rewards:
+        env = ClipRewardEnv(env)
+
+    env = PreprocessAtariObs(env)
+    env = FrameBuffer(env, n_frames=4, dim_order='pytorch')
     return env
