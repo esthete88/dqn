3af5a77c3406ae23d1ec75d362577c4497344467
esthete88 <nordmtr@gmail.com>
2020-04-08 00:30:04 +0300

    play and record

---
+++ player.py
@@ -0,0 +1,26 @@
+def play_and_record(initial_state, agent, env, exp_replay=None, n_steps=1):
+    """
+    Play the game for exactly n steps, record every (s,a,r,s', done) to replay buffer.
+    Whenever game ends, add record with done=True and reset the game.
+    It is guaranteed that env has done=False when passed to this function.
+
+    :returns: return sum of rewards over time and the state in which the env stays
+    """
+    total_reward = 0.0
+    s = initial_state
+
+    for _ in range(n_steps):
+        # get agent to pick action given state s
+        a = agent.get_actions([s])[0]
+        next_s, r, done, _ = env.step(a)
+
+        if exp_replay is not None:
+            # store current <s,a,r,s'> transition in buffer
+            exp_replay.add(s, a, r, next_s, done)
+
+        s = next_s
+        total_reward += r
+        if done:
+            s = env.reset()
+
+    return total_reward, s
